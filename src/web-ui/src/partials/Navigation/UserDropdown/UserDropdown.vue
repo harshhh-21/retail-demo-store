425ec088d4ab8e213821e1a26a20dae0137dbd0a
Harsh Mevada <harsh@Harsh.localdomain>
2026-06-04 08:55:58 +0000

    checking CD

--- src/web-ui/src/partials/Navigation/UserDropdown/UserDropdown.vue
+++ src/web-ui/src/partials/Navigation/UserDropdown/UserDropdown.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <router-link v-if="!user" to="/auth" class="user-dropdown-button login-button btn">Check IN</router-link>
+    <router-link v-if="!user" to="/auth" class="user-dropdown-button login-button btn">Log In</router-link>
 
     <button
       v-if="user"
